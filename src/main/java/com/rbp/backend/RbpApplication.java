e96dfea7e0a6552862d2fbf988519a8cd229cbe0
ascsoftdev <ascsoftdev@gmail.com>
2020-05-03 17:26:48 +0530

    102: allowed cross origins

--- src/main/java/com/rbp/backend/RbpApplication.java
+++ src/main/java/com/rbp/backend/RbpApplication.java
@@ -2,6 +2,9 @@ package com.rbp.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RbpApplication {
@@ -9,6 +12,18 @@ public class RbpApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RbpApplication.class, args);
 		System.out.println("Ajay");
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/states").allowedOrigins("http://localhost:4200");
+			}
+			
+		};
 	}
 
 }
